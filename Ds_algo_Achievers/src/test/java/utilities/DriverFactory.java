b92483ad84d84f86f28e4c101e3ea6c8f3fcbdf1
VJyothsna23 <124528683+VJyothsna23@users.noreply.github.com>
2023-12-04 20:06:27 -0800

    First Commit..

---
+++ Ds_algo_Achievers/src/test/java/utilities/DriverFactory.java
@@ -0,0 +1,51 @@
+package utilities;
+
+import java.time.Duration;
+
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverFactory {
+	
+	public static WebDriver driver;
+	public static String browser;
+	
+	public static WebDriver initializeDriver(String browser) {
+		
+		if(browser == "Chrome"){
+			
+			ChromeOptions co = new ChromeOptions();
+			co.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+			
+			driver = new ChromeDriver(co);
+			
+		}
+		
+		else if(browser == "Edge"){
+			
+			driver = new EdgeDriver();
+		}
+		
+		else if(browser == "FireFox") {
+			
+			driver = new FirefoxDriver();
+		}
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		return driver;
+			
+		}
+	
+	public static void closeDriver() {
+		
+		driver.close();
+//		driver.quit();
+		
+	}
+
+}
